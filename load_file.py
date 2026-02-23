4d7d67557590adc0dfdaeeedb8cb3b63f33e0a99
yokomizo-groove <60562107+yokomizo-groove@users.noreply.github.com>
2026-02-23 16:01:26 +0900

    Update load_file.py

--- load_file.py
+++ load_file.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 
+print ("Load File ...")
 
 def load_file(uploaded_file, MAX_COL=150):
 
@@ -64,3 +65,4 @@ def load_file(uploaded_file, MAX_COL=150):
 
     else:
         raise ValueError("Unsupported file format")
+
