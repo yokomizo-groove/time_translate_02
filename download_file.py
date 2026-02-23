cd9671748f2ff763f3d322e598424cd712f26295
yokomizo-groove <60562107+yokomizo-groove@users.noreply.github.com>
2026-02-23 17:05:32 +0900

    Update download_file.py

--- download_file.py
+++ download_file.py
@@ -1,6 +1,9 @@
 from io import BytesIO
 import streamlit as st
 
+import io
+import pandas as pd
+
 
 
 def download_file(df):
@@ -11,5 +14,16 @@ def download_file(df):
     df.to_excel(output, index=False, engine="xlsxwriter")
     output.seek(0)
     return output
+
+def to_excel_xlsxwriter(df):
+    output = io.BytesIO()
+
+    # xlsxwriter をエンジンとして使う
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+
+        # writer.save() は不要（with が自動でやる）
+
+    return output.getvalue()
 
 
