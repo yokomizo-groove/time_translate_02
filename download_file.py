ca346f474302f2340fdd969fa7978b7a195ca965
yokomizo-groove <60562107+yokomizo-groove@users.noreply.github.com>
2026-02-23 16:07:11 +0900

    Update download_file.py

--- download_file.py
+++ download_file.py
@@ -1,8 +1,11 @@
 from io import BytesIO
+import streamlit as st
 
+st.write("Download File ...")
 
 def download_file(df):
     output = BytesIO()
     df.to_excel(output, index=False, engine="xlsxwriter")
     output.seek(0)
     return output
+
