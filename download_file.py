eaa5ae1ac0586303b2e6cf99d2945d346ff9d820
yokomizo-groove <60562107+yokomizo-groove@users.noreply.github.com>
2026-02-24 09:03:21 +0900

    Update download_file.py

--- download_file.py
+++ download_file.py
@@ -18,14 +18,32 @@ def download_file(df):
 def to_excel_xlsxwriter(df):
     output = io.BytesIO()
 
-    st.write("Making download file with xlsxwriter...")
-    
     # xlsxwriter をエンジンとして使う
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
         df.to_excel(writer, index=False, sheet_name='Sheet1')
 
         # writer.save() は不要（with が自動でやる）
 
+    return output.getvalue()
+
+def to_excel_fast_numpy(final_array, headers):
+    output = io.BytesIO()
+    import xlsxwriter
+
+    workbook = xlsxwriter.Workbook(output, {'in_memory': True})
+    worksheet = workbook.add_worksheet("Sheet1")
+
+    # ヘッダー書き込み
+    for col, name in enumerate(headers):
+        worksheet.write(0, col, name)
+
+    # データ書き込み（NumPy 配列を直接書く）
+    rows, cols = final_array.shape
+    for r in range(rows):
+        for c in range(cols):
+            worksheet.write(r + 1, c, final_array[r, c])
+
+    workbook.close()
     return output.getvalue()
 
 
