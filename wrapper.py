1306fe66f6f17059e145233f3a159f8be756f597
yokomizo-groove <60562107+yokomizo-groove@users.noreply.github.com>
2026-03-10 11:09:38 +0900

    wrapper.py の更新

--- wrapper.py
+++ wrapper.py
@@ -4,3 +4,6 @@ import streamlit.web.cli as stcli
 if __name__ == "__main__":
     sys.argv = ["streamlit", "run", "main.py", "--server.headless=true"]
     sys.exit(stcli.main())
+
+# exe化コマンドライン
+# pyinstaller --noconsole --onefile wrapper.py
